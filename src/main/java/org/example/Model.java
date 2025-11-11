8652e5895f8e04e658af055fcea41f2e7a54b35f
AlexanderTurns <erenhirabayashi@gmail.com>
2025-11-11 18:40:01 +0300

    Update Model.java

--- src/main/java/org/example/Model.java
+++ src/main/java/org/example/Model.java
@@ -1,1 +1,17 @@
+public class Lamp{
+  private int globRating;
+  private boolean battery;
+  private Lamptype style;
+  public Lamp(int g,boolean b,Lamptype s){
+    this.globRating = g;
+    this.battery = b;
+    this.style = s;
+  }
+
+public enum Lamptype{
+  NORMAL,
+  NEON,
+  LAVA;
+}
+}
 
